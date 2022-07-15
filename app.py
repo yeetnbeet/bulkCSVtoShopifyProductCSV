bb984d07c1f2ae59b71efdd95207dbd28aec898d
Ryan Littlefield <segalcsam@gmail.com>
2022-07-15 10:52:21 -0600

    small refactor

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from logging import PlaceHolder
 from typing import Counter
-from numpy import product
+from markupsafe import string
+from numpy import size
 import pandas as pd
 
+#This is the columns you want from the csv
+class columID :
+    title = 'ProductText'
+    sku = 'MaterialNumber'
+    build = 'Spec'
+    color = 'Color'
+    taille = 'Size' 
+
+identifiers = columID() 
+
 # get only the columns you want from the csv file
-df = pd.read_csv('DATA.CSV', usecols=['ProductText', 'MaterialNumber','Spec','Color','Size'])
+df = pd.read_csv('DATA.CSV', usecols=[identifiers.title,identifiers.sku,identifiers.build,identifiers.color,identifiers.taille])
 result = df.to_dict(orient='records')
 
 dictByBike ={}
 #Finds Variants From Duplicate Text and Parses them into an easy to read object (lists of variants)
-def cleanDic(dict):
+def cleanDic(dict,identifier=string):
     dictByBike ={} 
     a = []   
     for item in dict :
-        a.append(item["ProductText"])
+        a.append(item[identifier])
     res = []
     for i in a:
         if i not in res:
@@ -21,7 +32,7 @@ def cleanDic(dict):
     for i in res :
         l = []
         for obj in dict:
-            if i == obj['ProductText']:
+            if i == obj[identifier]:
                 l.append(obj)
         dictByBike[i]=l
 
@@ -30,5 +41,7 @@ def cleanDic(dict):
     print(dictByBike)
     return dictByBike
 
+
 if __name__ == "__main__":
-    cleanDic(result)
+    cleanDic(result,"ProductText")
+    
